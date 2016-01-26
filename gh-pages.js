7b7e48e4369a905656c3f64a36ab95b1ad8cf475
Fabian Trinler <fabian.trinler@blateral.com>
2016-01-26 17:18:27 +0100

    Fixing publish api call

--- gh-pages.js
+++ gh-pages.js
@@ -2,6 +2,6 @@ var ghpages = require('gh-pages');
 var path = require('path');
 var pkg = require('./package.json');
 
-ghpages.publish(path.join(__dirname, 'dist'), function(err) {
+ghpages.publish(path.join(__dirname, 'dist'), {
     message: 'Bumping to ' + pkg.version
-});
+}, function(err) {});
