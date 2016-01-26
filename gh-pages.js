0d075501ba468fccc8bead2265fd525fe0d40829
Fabian Trinler <fabian.trinler@blateral.com>
2016-01-26 17:12:51 +0100

    Fixing require statement

--- gh-pages.js
+++ gh-pages.js
@@ -1,6 +1,6 @@
 var ghpages = require('gh-pages');
 var path = require('path');
-var pkg: require('./package.json');
+var pkg = require('./package.json');
 
 ghpages.publish(path.join(__dirname, 'dist'), function(err) {
     message: 'Bumping to ' + pkg.version
